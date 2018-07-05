f614c18c54aca736a8a9b91813603faeea229a08
vikas1982 <bhutani.vikas1982@hotmail.com>
2018-07-05 16:28:53 +0530

    con

--- deploy.cmd
+++ deploy.cmd
@@ -107,9 +107,18 @@ IF !ERRORLEVEL! NEQ 0 goto error
 popd
 )
  
+:: 4. Copy Web.config
+IF EXIST "%DEPLOYMENT_SOURCE%\web.config" (
+  pushd "%DEPLOYMENT_SOURCE%"
+ :: the next line is optional to fix 404 error see section #8
+  call :ExecuteCmd cp web.config dist\
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+ 
 :: 5. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
